3033b81c00910c628cb496201fc5e522929268c1
Pazzik <andrej-bulygin@yandex.ru>
2016-10-24 15:36:09 +0300

    change sinatra to socket

--- app.rb
+++ app.rb
@@ -1,12 +1,7 @@
 require 'geokit'
 require "nearest_time_zone"
 require 'tzinfo'
-require 'sinatra'
-
-get '/time' do
-  locations = params.keys[0].to_s
-  get_time(locations).map{ |line| "<p>#{line}</p>" }.join
-end
+require "socket" 
 
 def get_time(locations)
   time = Time.new.utc.strftime "%Y-%m-%d %H:%M:%S" 
@@ -21,4 +16,22 @@ def get_time(locations)
   end
 
   return result
-end
+end 
+
+Socket.tcp_server_loop(3000) {|sock, client_addrinfo|
+  Thread.new {
+    begin
+      message = sock.recvmsg[0]
+      first_line = message.lines[0]
+      query_string = first_line.split(' ')[1]
+      return if /\A\/time(\z|\?)/.match(query_string).nil? #check '/time' query
+      
+      locations = query_string.split('?')[1].to_s
+      resp = get_time(locations).map{ |line| "<p>#{line}</p>" }.join
+
+      sock.write(resp)
+    ensure
+      sock.close
+    end
+  }
+}
