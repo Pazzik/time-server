960e269300d0a7a9e79dc7a7e2349bb9ce6ae1d8
Pazzik <andrej-bulygin@yandex.ru>
2018-12-07 10:29:27 +0300

    refactoring

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'geokit'
 gem 'nearest_time_zone'
-gem 'tzinfo'
+gem 'rubocop'
 gem 'sinatra'
+gem 'tzinfo'
--- app.rb
+++ app.rb
@@ -1,24 +1,28 @@
 require 'geokit'
-require "nearest_time_zone"
+require 'nearest_time_zone'
 require 'tzinfo'
 require 'sinatra'
 
 get '/time' do
   locations = params.keys[0].to_s
-  get_time(locations).map{ |line| "<p>#{line}</p>" }.join
+  get_time(locations).map { |line| "<p>#{line}</p>" }.join
 end
 
 def get_time(locations)
-  time = Time.new.utc.strftime "%Y-%m-%d %H:%M:%S" 
+  time = Time.new.utc.strftime '%Y-%m-%d %H:%M:%S'
   result = ["UTC: #{time}"]
 
   locations.split(',').compact.each do |location|
-    res = Geokit::Geocoders::GoogleGeocoder.geocode(location)
-    timezone_name = NearestTimeZone.to(res.lat,res.lng)
-    zone = TZInfo::Timezone.get(timezone_name) 
-    time = zone.now.strftime "%Y-%m-%d %H:%M:%S" 
+    time = time_for_location(location)
     result << "#{location.strip}: #{time}"
   end
 
-  return result
+  result
+end
+
+def time_for_location(location)
+  res = Geokit::Geocoders::GoogleGeocoder.geocode(location)
+  timezone_name = NearestTimeZone.to(res.lat, res.lng)
+  zone = TZInfo::Timezone.get(timezone_name)
+  zone.now.strftime '%Y-%m-%d %H:%M:%S'
 end
